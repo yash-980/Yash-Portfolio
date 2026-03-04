90836a1ef3550e598c0a998bed7e6575a2b296b7
yash-980 <kolteyash980@gmail.com>
2026-03-04 17:52:44 +0530

    Made Some chaanges In INtro Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,23 @@ import { useState } from "react";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import About from "./components/About";
-import Experience from "./components/Experience"; 
-import Intro from "./components/Intro";
+import Experience from "./components/Experience";
 
 export default function App() {
-  const [showIntro, setShowIntro] = useState(true);
-  return (
 
+  return (
     <>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-             {showIntro && <Intro onFinish={() => setShowIntro(false)} />}
-
-  
-        <div className="main-content">
-        <Header />
-          <HeroSection />
-          <About />
-          <Experience/>
-          <h1>Welcome to My Portfolio</h1>
+          <div className="main-content ">
+            <Header />
+            <HeroSection />
+            <About />
+            <Experience/>
           </div>
         </div>
       </div>
     </>
   )
 }
-
-
-
 
